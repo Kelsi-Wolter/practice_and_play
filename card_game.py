19a009d83e5b105781157d2557df2daedd21385a
Kelsi-Wolter <kelsi.wolter@gmail.com>
2021-01-12 19:09:24 +0000

    Created deck and cards classes to create deck of cards

--- card_game.py
+++ card_game.py
@@ -2,22 +2,30 @@
 # Now design a class to represent a deck of cards. 
 # Using these two classes, implement a favorite card game.
 
-class Card(self):
+class Card:
     def __init__(self, value, suite):
         self.value = value
         self.suite = suite
     
     def __repr__(self):
-        return f'<Card: Value={self.value}, Suite={self.suite}>'
+        return f'<Card: {self.value} of {self.suite}>'
 
 
-class DeckofCards(self):
+class DeckofCards:
     def __init__(self):
         self.cards = []
     
     def create_deck(self):
-        for suite in ['hearts', 'spades', 'diamonds', 'clubs']
+        for suite in ['hearts', 'spades', 'diamonds', 'clubs']:
             for num in range(2,11):
                 card = Card(num, suite)
-                DeckofCards.cards.append(card)
+                self.cards.append(card)
+            for face in ['king', 'queen','jack', 'ace']:
+                card = Card(num, suite)
+                self.cards.append(card)
+    
+    def __repr__(self):
+        deck = len(self.cards)
+        return f'<DeckofCards: {deck} cards currently in deck>'
+
 
